b8fab954d1007be8b8b63d82a4e9f7b6a8c62821
zbaker94 <zbaker94@gmail.com>
2026-03-15 23:00:54 -0400

    simplify play section with simple kill button

--- src/components/composite/SidePanel/PlaySection.tsx
+++ src/components/composite/SidePanel/PlaySection.tsx
@@ -1,21 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { PlayIcon, SmileDizzyIcon } from "@hugeicons/core-free-icons";
+import {SmileDizzyIcon } from "@hugeicons/core-free-icons";
 import { usePlaybackStore } from "@/state/playbackStore";
-import { useCallback } from "react";
 
 export function PlaySection() {
   const setIsPlaying = usePlaybackStore((s) => s.setIsPlaying);
   const isPlaying = usePlaybackStore((s) => s.isPlaying);
 
-  const togglePlay = useCallback(() => {
-    setIsPlaying(!isPlaying);
-  }, [isPlaying, setIsPlaying]);
 
   return (
     <div className="flex items-center p-1 md:pb-2">
-      <Button variant={isPlaying ? "destructive" : "default"} size="icon-lg" className="size-11 md:size-9 p-0" onClick={togglePlay}>
-        <HugeiconsIcon size={200} icon={isPlaying ? SmileDizzyIcon : PlayIcon} />
+      <Button disabled={!isPlaying} variant={"destructive"} size="icon-lg" className="size-11 md:size-9 p-0" onClick={() => setIsPlaying(false)}>
+        <HugeiconsIcon  icon={SmileDizzyIcon} />
       </Button>
     </div>
   );
