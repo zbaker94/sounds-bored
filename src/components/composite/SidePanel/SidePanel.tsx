5c6b081565a6c500232b3d8988d2c5be23055cde
zbaker94 <zbaker94@gmail.com>
2026-03-31 09:26:00 -0400

    update right side panel image

--- src/components/composite/SidePanel/SidePanel.tsx
+++ src/components/composite/SidePanel/SidePanel.tsx
@@ -4,7 +4,7 @@ import { PlaySection } from "./PlaySection";
 
 export function SidePanel() {
   return (
-    <aside className="h-16 w-full shrink-0 bg-yellow-500 drop-shadow-[0_-5px_0px_rgba(0,0,0,1)] flex flex-row md:h-full md:w-12 md:drop-shadow-[-5px_0_0px_rgba(0,0,0,1)] md:flex-col md:justify-between bricked-yellow-500-overlay">
+    <aside className="h-16 w-full shrink-0 bg-yellow-500 drop-shadow-[0_-5px_0px_rgba(0,0,0,1)] flex flex-row md:h-full md:w-12 md:drop-shadow-[-5px_0_0px_rgba(0,0,0,1)] md:flex-col md:justify-between illuminated-yellow-500-overlay">
       <EditSection />
       <VolumeSection />
       <PlaySection />
